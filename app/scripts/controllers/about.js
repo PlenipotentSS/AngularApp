89ab517c8c43390b35331dd2b9f3a2b7fa556794
plenipotentSS <stevensonhoyt@icloud.com>
2015-07-11 15:43:26 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('AboutCtrl', ['$scope', function ($scope) {
+    this.awesomeThings = [
+          'HTML5 Boilerplate',
+          'AngularJS',
+          'Karma'
+    ];
+
+    $scope.title ="This song is crazy!";
+
+  }]);
