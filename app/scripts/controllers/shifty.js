0f9ba892f5c3b4f96b2eb124204deb29940b5a83
plenipotentSS <stevensonhoyt@icloud.com>
2015-07-12 13:02:08 -0700

    added rebuild for shifty

--- app/scripts/controllers/shifty.js
+++ app/scripts/controllers/shifty.js
@@ -15,61 +15,81 @@ angular.module('angularAppApp')
       'Karma'
     ];
 
-    var TA = []; // Array of tweenables;
-    var TP = []; // Current state;
-    var TE = []; // Elements;
+    var scene;
 
-    for (var i=1; i<=20; i++) {
-      TE[i] = document.getElementById('fig' + i.toString());
-      TA[i] = new Tweenable();
-      TA[i].setConfig({     
+    var Tweening = function() {
+      var _this = this;
+      this.TA = []; // Array of tweenables;
+      this.TP = []; // Current state;
+      this.TE = []; // Elements;
+      this.interval;
 
-        // We attach (and thus, cache) the corresponding DOM element for each Tweenable insance;
-        attachment : [i, TE[i]],
-
-        from : {
-          'x' : 0,
-          'y' : 0,
-          'rot' : 0,
-          'scale' : 1.0,
-          'backgroundColor' : '#ff0000'
-        },        
-
-        to : {
-          'x' : 320 * Math.random() - 160,
-          'y' : 480* Math.random() - 240,
-          'rot' : Math.random() * 360,
-          'scale' : Math.random() * 4.5 + 0.5,
-          'backgroundColor' : '#ffff00'
-        },        
-
-        duration : (2000 * Math.random() + 500),        
-
-        step : function(currentState, currentAttachment) {       
-          if (!currentAttachment) 
-            return;                               
-          TP[currentAttachment[0]] = currentState;                        
-        },        
-
-        easing : 'bounce'
-
-      });
-    }
-
-    for (var i=1; i<=20; i++) {
-      TA[i].tween();
-    }
-
-    setInterval(function(){
-      for (var i=1; i<=20; i++) {
-        var ce = TE[i];
-        var cp = TP[i];
-        ce.style.transform = 
-        ce.style.msTransform = 
-        ce.style.mozTransform = 
-        ce.style.webkitTransform = 
-          'translate(' + cp.x + 'px, ' + cp.y + 'px) scale(' + cp.scale + ') rotate(' + cp.rot + 'deg)';
-        ce.style.backgroundColor = cp.backgroundColor;  
+      this.destroy = function() {
+        if (_this.interval) {
+          clearInterval(_this.interval);
+        }
       }
-    }, 1000/60);
+
+      for (var i=1; i<=20; i++) {
+        _this.TE[i] = document.getElementById('fig' + i.toString());
+        _this.TA[i] = new Tweenable();
+        _this.TA[i].setConfig({     
+
+          // We attach (and thus, cache) the corresponding DOM element for each Tweenable insance;
+          attachment : [i, _this.TE[i]],
+
+          from : {
+            'x' : 0,
+            'y' : 0,
+            'rot' : 0,
+            'scale' : 1.0,
+            'backgroundColor' : '#ff0000'
+          },        
+
+          to : {
+            'x' : 320 * Math.random() - 160,
+            'y' : 480* Math.random() - 240,
+            'rot' : Math.random() * 360,
+            'scale' : Math.random() * 4.5 + 0.5,
+            'backgroundColor' : '#ffff00'
+          },        
+
+          duration : (2000 * Math.random() + 500),        
+
+          step : function(currentState, currentAttachment) {       
+            if (!currentAttachment) {
+              return;                
+            }               
+            _this.TP[currentAttachment[0]] = currentState;                        
+          },        
+
+          easing : 'bounce'
+
+        });
+      }
+
+      for (var j=1; j<=20; j++) {
+        _this.TA[j].tween();
+      }
+
+      _this.interval = setInterval(function(){
+        for (var i=1; i<=20; i++) {
+          var ce = _this.TE[i];
+          var cp = _this.TP[i];
+          ce.style.transform = 
+          ce.style.msTransform = 
+          ce.style.mozTransform = 
+          ce.style.webkitTransform = 
+            'translate(' + cp.x + 'px, ' + cp.y + 'px) scale(' + cp.scale + ') rotate(' + cp.rot + 'deg)';
+          ce.style.backgroundColor = cp.backgroundColor;  
+        }
+      }, 1000/60);
+    };
+
+    scene = new Tweening();
+
+    $scope.rerun = function() {
+      scene.destroy();
+      scene = new Tweening();
+    }
   }]);
